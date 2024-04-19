dcb9ff347b9fe802f6b6209545398bcc73e013b8
Ferrett27 <161031706+Ferrett27@users.noreply.github.com>
2024-04-19 10:37:14 -0300

    Update multiplos-de-13.py

--- AC7/multiplos-de-13.py
+++ AC7/multiplos-de-13.py
@@ -1,11 +1,14 @@
-nums = []
 a = int(input())
 b = int(input())
 
-for i in range(a, b+1):
-  if i % 13 != 0:
-    nums.append(i)
+def mult_13(a, b):
+    nums = []
+    for i in range(a, b+1):
+        if i % 13 != 0:
+            nums.append(i)
+    total = sum(nums)
+    return total
 
-resul = sum(nums)
+resul = mult_13(a, b)
 
 print(resul)
