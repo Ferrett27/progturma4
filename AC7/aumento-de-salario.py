a84c33f2852b3a35ca5c88b36c7d9cc392aaec92
Ferrett27 <161031706+Ferrett27@users.noreply.github.com>
2024-04-19 11:04:30 -0300

    Update aumento-de-salario.py

--- AC7/aumento-de-salario.py
+++ AC7/aumento-de-salario.py
@@ -1,36 +1,41 @@
 salario = float(input())
-if 0 < salario <= 400.00:
-    percentual = 15
-    salarionovo = ((salario * percentual) / 100) + salario
-    reajuste = salarionovo - salario
+
+if 0 < salario <= 400:
+    per = 15
+    salarionovo = salario + ((salario * per) / 100)
+    reajs = salarionovo - salario
     print("Novo salario: %.2f" %salarionovo)
-    print("Reajuste ganho: %.2f" %reajuste)
-    print(f"Em percentual: {percentual} %")
-elif 400.01 < salario <= 800.00:
-    percentual = 12
-    salarionovo = ((salario * percentual) / 100) + salario
-    reajuste = salarionovo - salario
+    print("Reajuste ganho: %.2f" %reajs)
+    print(f"Em percentual: {per} %")
+
+elif 400.01 <= salario <= 800:
+    per = 12
+    salarionovo = salario + ((salario * per) / 100)
+    reajs = salarionovo - salario
     print("Novo salario: %.2f" %salarionovo)
-    print("Reajuste ganho: %.2f" %reajuste)
-    print(f"Em percentual: {percentual} %")
-elif 800.01 < salario <= 1200.00:
-    percentual = 10
-    salarionovo = ((salario * percentual) / 100) + salario
-    reajuste = salarionovo - salario
+    print("Reajuste ganho: %.2f" %reajs)
+    print(f"Em percentual: {per} %")
+
+elif 800.01 <= salario <= 1200:
+    per = 10
+    salarionovo = salario + ((salario * per) / 100)
+    reajs = salarionovo - salario
     print("Novo salario: %.2f" %salarionovo)
-    print("Reajuste ganho: %.2f" %reajuste)
-    print(f"Em percentual: {percentual} %")
-elif 1200.01 < salario <= 2000.00:
-    percentual = 7
-    salarionovo = ((salario * percentual) / 100) + salario
-    reajuste = salarionovo - salario
+    print("Reajuste ganho: %.2f" %reajs)
+    print(f"Em percentual: {per} %")
+
+elif 1200.01 <= salario <= 2000.00:
+    per = 7
+    salarionovo = salario + ((salario * per) / 100)
+    reajs = salarionovo - salario
     print("Novo salario: %.2f" %salarionovo)
-    print("Reajuste ganho: %.2f" %reajuste)
-    print(f"Em percentual: {percentual} %")
-elif salario > 2000.0:
-    percentual = 4
-    salarionovo = ((salario * percentual) / 100) + salario
-    reajuste = salarionovo - salario
+    print("Reajuste ganho: %.2f" %reajs)
+    print(f"Em percentual: {per} %")
+
+elif salario > 2000.01:
+    per = 4
+    salarionovo = salario + ((salario * per) / 100)
+    reajs = salarionovo - salario
     print("Novo salario: %.2f" %salarionovo)
-    print("Reajuste ganho: %.2f" %reajuste)
-    print(f"Em percentual: {percentual} %")
+    print("Reajuste ganho: %.2f" %reajs)
+    print(f"Em percentual: {per} %")
