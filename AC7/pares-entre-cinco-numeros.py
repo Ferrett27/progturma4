f43c7eabf77c7980761ffcb3670e5ddbc5df4885
Ferrett27 <161031706+Ferrett27@users.noreply.github.com>
2024-04-19 10:39:22 -0300

    Update pares-entre-cinco-numeros.py

--- AC7/pares-entre-cinco-numeros.py
+++ AC7/pares-entre-cinco-numeros.py
@@ -1,6 +1,11 @@
-pares = 0
-for i in range(5):
-    num = int(input())
-    if num % 2 == 0:
-        pares += 1
+def pares_5():
+    pares = 0
+    for i in range(5):
+        num = int(input())
+        if num % 2 == 0:
+            pares += 1
+    return pares
+
+pares = pares_5()
+
 print(f"{pares} valores pares")
